111a2a130fc3041d847410bea14aa6937f6825e1
Vidura Priyadarshana <vidura200427@gmail.com>
2025-09-13 13:17:56 +0530

    feat(ui): complete UI implementation for [home/card/nav]

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import NavBar from "~/components/NavBar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "const";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback for your dream joob" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+    <NavBar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and cheack AI-powered feedback.</h2>
+      </div>
+
+      {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+      )}
+      
+    </section>
+  </main>;
 }
