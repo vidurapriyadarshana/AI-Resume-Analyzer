7868dbedcdca0eefbd324298ab3f0d6b4f0bd6ea
Vidura Priyadarshana <vidura200427@gmail.com>
2025-09-15 14:46:49 +0530

    feat: resume feedback components impl

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,3 +1,6 @@
+import {type ClassValue, clsx} from "clsx";
+import {twMerge} from "tailwind-merge";
+
 export function formatSize(bytes: number): string {
     if (bytes === 0) return '0 Bytes';
 
@@ -9,3 +12,9 @@ export function formatSize(bytes: number): string {
 }
 
 export const genarateUUID = (): string => crypto.randomUUID();
+
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
